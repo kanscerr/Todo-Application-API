4df547a25fb9f6f445e768ace0d0cf853549c590
kanscerr <92222438+kanscerr@users.noreply.github.com>
2022-05-09 20:40:22 +0530

    user-friendly response

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,6 @@
 //importing express.js, mongo model
 const express = require('express');
+const { resourceLimits } = require('worker_threads');
 const model = require('../schema');
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.post('/', (req, res) => {
         todo : req.body.todo
     });
     newTask.save()
-    res.json(newTask);
+    res.send(`New task added: ${req.body.todo}`);
 });
 
 //route for deleting a todo
@@ -18,10 +19,10 @@ router.delete('/:task', (req, res) => {
     const deleteId = req.params.task;
     model.findOneAndDelete({task : deleteId}, (error, result) => {
         if(!result){
-            res.send("Given task does not exist in database!");
+            res.send("Given task does not exist!");
         }
         else{
-            res.send(`Deleted: ${JSON.stringify(result)}`);
+            res.send(`You deleted task ${req.params.task} : ${result.todo}`);
         }
     })
 })
@@ -30,10 +31,26 @@ router.delete('/:task', (req, res) => {
 router.get('/', (req, res) => {
     model.find((error, result) => {
         if(!result){
-            res.send("No task available in database!");
+            res.send("No task available");
         }
         else{
-            res.json(result);
+            let todoString = '';
+            model.countDocuments({}).exec((err, result) => {
+                if(err){
+                    res.send(err);
+                }
+                else{
+                    const num = result;
+                    model.find().sort({task : 1}).exec((error, result) => {
+                        if(result){
+                            for(i=0; i<num;i++){
+                                todoString += "Task " + result[i].task + " - " + result[i].todo + "\n"
+                            }
+                            res.send(todoString);
+                        }
+                    });
+                }
+            })
         }
     })
 })
@@ -43,10 +60,10 @@ router.get('/:task', (req, res) => {
     const taskID = req.params.task;
     model.findOne(({task : taskID}), (error, result) => {
         if(!result){
-            res.send("No such task available in database!");
+            res.send("No such task available!");
         }
         else{
-            res.json(result);
+            res.send(`Task ${taskID} - ${result.todo}`);
         }
     })
 })
@@ -56,13 +73,13 @@ router.put('/:task', (req, res) => {
     const taskID = req.params.task;
     model.findOneAndUpdate(
         {task : taskID},
-        {task : req.body.task, todo : req.body.todo},
+        {todo : req.body.todo},
         (error, result) => {
         if(!result){
-            res.send('No such task available in database!')
+            res.send('No such task available!')
         }
         else{
-            res.send(`task ${taskID} updated to ${JSON.stringify(req.body)}`);
+            res.send("Task " + taskID + " updated- \n" + "Task " + req.body.task + " - " + req.body.todo);
         }
     })
 })
